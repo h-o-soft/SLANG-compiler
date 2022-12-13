0f755fce673ab4c59c8948144d781e56cdd815b1
Hiroshi OGINO <hiroshi.ogino@lyles.jp>
2022-12-13 23:31:57 +0900

    copyruntime.batを修正

--- copyruntime.bat
+++ copyruntime.bat
@@ -1,6 +1,7 @@
 ECHO OFF
 
 mkdir %homedrive%%homepath%\.config\SLANG\
+mkdir %homedrive%%homepath%\.config\SLANG\extlib
 copy *.env %homedrive%%homepath%\.config\SLANG\
 copy *.yml %homedrive%%homepath%\.config\SLANG\
-xcopy /E extlib %homedrive%%homepath%\.config\SLANG\
+xcopy /E /Y extlib %homedrive%%homepath%\.config\SLANG\extlib
